75863c7b5ce59307b9adfef586487a9eb090a89c
raynos <raynos2@gmail.com>
2016-02-20 20:19:43 -0800

    add bunch of skipped tests

--- test/batch-client-new.js
+++ test/batch-client-new.js
@@ -20,6 +20,19 @@ test('working new call', function t(assert) {
     assert.end();
 });
 
+test('calling constructor with wrong type');
+test('calling constructor with too many args');
+test('calling constructor with too few args');
+test('calling new on normal function');
+test('calling new on non-object thisArg');
+test('calling new on empty object thisArg');
+test('assigning result of new to wrong type');
+test('calling new on constructor with a return string');
+test('calling new on constructor with a return other object');
+test('calling new on constructor with return null');
+test('calling new on constructor with return undefined');
+test('calling new on constructor with an empty return');
+
 function getFile(fileName) {
     return path.join(batchClientDir, fileName);
 }
