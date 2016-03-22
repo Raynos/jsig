f6d9253e46107b562ac895b80e7f8bcec5d50596
raynos <raynos2@gmail.com>
2016-03-21 22:53:34 -0700

    stub out more test cases

--- test/batch-client-modules.js
+++ test/batch-client-modules.js
@@ -7,7 +7,7 @@ var compile = require('../type-checker/');
 
 var batchClientDir = path.join(__dirname, 'batch-client-modules');
 
-test('working method calls within a closure', function t(assert) {
+test('working require from another file', function t(assert) {
     var file = getFile('good-working-require1.js');
 
     var meta = compile(file);
@@ -17,6 +17,20 @@ test('working method calls within a closure', function t(assert) {
 
     assert.end();
 });
+
+test('working require side effect file');
+
+test('require non-existant file');
+test('require file with ES syntax error');
+test('require file that does not export anything');
+test('require file that does not type check');
+test('require function from file and call incorrectly');
+
+test('import non-existant file in hjs');
+test('import header file with jsig syntax error');
+test('import header file that does not define token');
+test('import header file but no tokens');
+test('redefine token imported from header file');
 
 function getFile(fileName) {
     return path.join(batchClientDir, fileName);
