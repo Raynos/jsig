54db68a9000db11ea66d9c86e1ff7f176410ade1
Raynos <raynos2@gmail.com>
2016-03-31 00:50:15 -0700

    more tests

--- test/index.js
+++ test/index.js
@@ -5,3 +5,4 @@ require('./batch-client-methods.js');
 require('./batch-client-new.js');
 require('./batch-client-calls.js');
 require('./batch-client-modules.js');
+require('./proc-watcher-node_modules.js');
