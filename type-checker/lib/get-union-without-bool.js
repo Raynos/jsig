e23067e33e8c3a816a75be5909f4ed27044546df
Raynos <raynos2@gmail.com>
2016-04-02 03:44:47 -0700

    move union without bools into helper file

---
+++ type-checker/lib/get-union-without-bool.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var JsigAST = require('../../ast.js');
+
+module.exports = getUnionWithoutBool;
+
+function getUnionWithoutBool(type, truthy) {
+    if (type.type !== 'unionType') {
+        if (truthy && !isAlwaysFalsey(type)) {
+            return type;
+        } else if (!truthy && !isAlwaysTruthy(type)) {
+            return type;
+        }
+
+        return null;
+    }
+
+    var unions = [];
+    for (var i = 0; i < type.unions.length; i++) {
+        var t = type.unions[i];
+        if (
+            (truthy && !isAlwaysFalsey(t)) ||
+            (!truthy && !isAlwaysTruthy(t))
+        ) {
+            unions.push(t);
+        }
+    }
+
+    if (unions.length === 1) {
+        return unions[0];
+    }
+
+    return JsigAST.union(unions, type.label);
+}
+
+// handle more literals like 0 or "" or false
+function isAlwaysTruthy(t) {
+    return !(
+        (t.type === 'valueLiteral' && t.name === 'undefined') ||
+        (t.type === 'valueLiteral' && t.name === 'null') ||
+        (t.type === 'typeLiteral' && t.name === 'String') ||
+        (t.type === 'typeLiteral' && t.name === 'Boolean') ||
+        (t.type === 'typeLiteral' && t.name === 'Number')
+    );
+}
+
+function isAlwaysFalsey(t) {
+    return (t.type === 'valueLiteral' && t.name === 'undefined') ||
+        (t.type === 'valueLiteral' && t.name === 'null');
+}
