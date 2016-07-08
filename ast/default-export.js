c4528f0c2038123712e32e1d30c62edd9c996291
Raynos <jakev@uber.com>
2016-07-07 23:20:42 -0700

    lint

--- ast/default-export.js
+++ ast/default-export.js
@@ -3,7 +3,7 @@
 module.exports = DefaultExportNode;
 
 function DefaultExportNode(expr) {
-    this.type ='defaultExport';
+    this.type = 'defaultExport';
     this.typeExpression = expr;
     this._raw = null;
 }
