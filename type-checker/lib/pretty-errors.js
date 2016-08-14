5bee7844e6fd24b36ae4a237dc582157a526d76a
Raynos <raynos2@gmail.com>
2016-08-14 05:04:13 -0700

    pretty: improved tracing pretty printing

--- type-checker/lib/pretty-errors.js
+++ type-checker/lib/pretty-errors.js
@@ -40,15 +40,21 @@ function prettyPrintAllErrorsWithTrace(checker) {
     for (var i = 0; i < checker.errors.length; i++) {
         var error = checker.errors[i];
 
+        parts.push(prettyPrintErrorStatement(checker, error));
+
         if (error.loc && isJSFileR.test(error.fileName)) {
             var extraTraces = findContextTraces(checker, error);
+
+            if (extraTraces.length > 0) {
+                parts.push('traces for error : ');
+                parts.push('');
+            }
+
             for (var j = 0; j < extraTraces.length; j++) {
                 var trace = extraTraces[j];
-                parts.push(prettyPrintTraceStatement(checker, trace));
+                parts.push(prettyPrintTraceStatement(checker, trace, '    '));
             }
         }
-
-        parts.push(prettyPrintErrorStatement(checker, error));
     }
 
     var word = checker.errors.length === 1 ? 'error' : 'errors';
@@ -65,20 +71,22 @@ function prettyPrintTraces(checker) {
 
     for (var i = 0; i < traces.length; i++) {
         var trace = traces[i];
-        parts.push(prettyPrintTraceStatement(checker, trace));
+        parts.push(prettyPrintTraceStatement(checker, trace, ''));
     }
 
     return parts.join('\n');
 }
 
-function prettyPrintTraceStatement(checker, trace) {
+function prettyPrintTraceStatement(checker, trace, prefix) {
     var str = '';
 
     assert(trace.fileName, 'trace must have fileName');
     var relativePath = path.relative(process.cwd(), trace.fileName);
 
-    str += TermColor.underline(relativePath) + '\n';
-    str += prettyPrintTrace(checker, trace);
+    str += prefix + TermColor.underline(relativePath) + '\n';
+    str += prettyPrintTrace(checker, trace, {
+        prefix: prefix || ''
+    });
 
     return str;
 }
@@ -161,9 +169,11 @@ function prettyPrintError(checker, error, opts) {
             sourceLines = meta.sourceLines;
         }
 
-        var failingLines = getFailingLines(sourceLines, error.loc);
+        var failingLines = getFailingLines(sourceLines, error.loc, {
+            prefix: opts.prefix
+        });
 
-        parts.push(opts.prefix + failingLines);
+        parts.push(failingLines);
         parts.push('');
     } else if (opts.showLoc !== false &&
         (
@@ -182,9 +192,11 @@ function prettyPrintError(checker, error, opts) {
                 column: sourceLines[error.line].length
             }
         };
-        failingLines = getFailingLines(sourceLines, loc);
+        failingLines = getFailingLines(sourceLines, loc, {
+            prefix: opts.prefix
+        });
 
-        parts.push(opts.prefix + failingLines);
+        parts.push(failingLines);
         parts.push('');
     }
 
@@ -218,14 +230,16 @@ function prettyPrintError(checker, error, opts) {
     return parts.join('\n');
 }
 
-function prettyPrintTrace(checker, trace) {
+function prettyPrintTrace(checker, trace, opts) {
     var parts = [];
 
-    parts.push('TRACE: ' + TermColor.cyan(trace.type));
+    parts.push(opts.prefix + 'TRACE: ' + TermColor.cyan(trace.type));
 
     if (trace.node.loc) {
         var meta = checker.getOrCreateMeta(trace.fileName);
-        var failingLines = getFailingLines(meta.sourceLines, trace.node.loc);
+        var failingLines = getFailingLines(meta.sourceLines, trace.node.loc, {
+            prefix: opts.prefix
+        });
 
         parts.push(failingLines);
         parts.push('');
@@ -234,14 +248,23 @@ function prettyPrintTrace(checker, trace) {
     var expected = serializeType(trace.expected);
     var actual = serializeType(trace.actual);
 
-    parts.push('Expected : ' + lineGreen(expected));
-    parts.push('Actual : ' + lineRed(actual));
+    var expectedLines = expected.split('\n');
+    var actualLines = actual.split('\n');
+    if (expectedLines.length > 1) {
+        expected = expectedLines.join('\n' + opts.prefix);
+    }
+    if (actualLines.length > 1) {
+        actual = actualLines.join('\n' + opts.prefix);
+    }
+
+    parts.push(opts.prefix + 'Expected : ' + lineGreen(expected));
+    parts.push(opts.prefix + 'Actual : ' + lineRed(actual));
     parts.push('');
 
     return parts.join('\n');
 }
 
-function getFailingLines(sourceLines, loc) {
+function getFailingLines(sourceLines, loc, opts) {
     var startLine = loc.start.line - 1;
     var endLine = loc.end.line - 1;
 
@@ -249,7 +272,7 @@ function getFailingLines(sourceLines, loc) {
     var nextLine = sourceLines[endLine + 1] || '';
 
     var segments = [
-        String(startLine) + '. ' + TermColor.gray(prevLine)
+        opts.prefix + String(startLine) + '. ' + TermColor.gray(prevLine)
     ];
 
     var failLine;
@@ -261,6 +284,7 @@ function getFailingLines(sourceLines, loc) {
         endColumn = loc.end.column;
 
         segments.push(
+            opts.prefix +
             String(startLine + 1) + '. ' +
             failLine.slice(0, startColumn) +
             TermColor.red(failLine.slice(startColumn, endColumn)) +
@@ -272,6 +296,7 @@ function getFailingLines(sourceLines, loc) {
         endColumn = loc.end.column;
 
         segments.push(
+            opts.prefix +
             String(startLine + 1) + '. ' +
             failLine.slice(0, startColumn) +
             TermColor.red(failLine.slice(startColumn, failLine.length))
@@ -279,6 +304,7 @@ function getFailingLines(sourceLines, loc) {
 
         for (var i = startLine + 1; i < endLine; i++) {
             segments.push(
+                opts.prefix +
                 String(i + 1) + '. ' + TermColor.red(sourceLines[i])
             );
         }
@@ -286,6 +312,7 @@ function getFailingLines(sourceLines, loc) {
         failLine = sourceLines[endLine];
 
         segments.push(
+            opts.prefix +
             String(endLine + 1) + '. ' +
             TermColor.red(failLine.slice(0, endColumn)) +
             failLine.slice(endColumn, failLine.length)
@@ -293,6 +320,7 @@ function getFailingLines(sourceLines, loc) {
     }
 
     segments.push(
+        opts.prefix +
         String(endLine + 2) + '. ' + TermColor.gray(nextLine)
     );
 
