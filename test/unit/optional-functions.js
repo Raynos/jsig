286c8bc4f4eed7f6ed5aab9476af1d924f61400a
raynos <raynos2@gmail.com>
2016-09-25 18:29:15 -0700

    improve unit test

--- test/unit/optional-functions.js
+++ test/unit/optional-functions.js
@@ -5,16 +5,17 @@ var JSIGSnippet = require('../lib/jsig-snippet.js');
 JSIGSnippet.test('allow optional function fields', {
     snippet: function m() {/*
         function Foo() {
-            this.x = null;
             this.y = null;
         }
 
         var f = new Foo();
-        f.x = function noop() {};
+        f.y = function noop() {};
+        f.y = noop;
+
+        function noop() {}
     */},
     header: function h() {/*
         type Foo : {
-            x: null | Function,
             y: null | (String) => String
         }
 
@@ -24,7 +25,7 @@ JSIGSnippet.test('allow optional function fields', {
     var meta = snippet.compile(assert);
 
     // TODO: fix error
-    assert.equal(meta.errors.length, 1);
+    assert.equal(meta.errors.length, 3);
 
     assert.end();
 });
