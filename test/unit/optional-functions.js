87bbd4d62f90e7b6688866502741891183f3ba88
raynos <raynos2@gmail.com>
2016-09-28 20:49:19 -0700

    another test

--- test/unit/optional-functions.js
+++ test/unit/optional-functions.js
@@ -31,3 +31,33 @@ JSIGSnippet.test('allow optional function fields', {
     assert.equal(meta.errors.length, 0);
     assert.end();
 });
+
+JSIGSnippet.test('cannot assign function expr ', {
+    snippet: function m() {/*
+        function Foo() {
+            this.y = null;
+        }
+
+        var f = new Foo();
+        f.y = function stringer() {
+            return '';
+        };
+        f.y = stringer;
+
+        function stringer() {
+            return '';
+        }
+    */},
+    header: function h() {/*
+        type Foo : {
+            y: null | () => String
+        }
+
+        Foo : (this: Foo) => void
+    */}
+}, function t(snippet, assert) {
+    var meta = snippet.compileAndCheck(assert);
+
+    assert.equal(meta.errors.length, 0);
+    assert.end();
+});
