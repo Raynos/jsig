0302f9fb161d6537ded502fa6ee9c1eaa22e52c1
raynos <raynos2@gmail.com>
2016-10-28 20:56:41 +0100

    tests: fix multiple return test

--- test/unit/multiple-returns.js
+++ test/unit/multiple-returns.js
@@ -18,13 +18,19 @@ JSIGSnippet.test('function with multiple returns', {
 }, function t(snippet, assert) {
     var meta = snippet.compile(assert);
 
-    assert.equal(meta.errors.length, 1);
+    assert.equal(meta.errors.length, 2);
 
     var err = meta.errors[0];
     assert.equal(err.type, 'jsig.sub-type.type-class-mismatch');
     assert.equal(err.expected, 'String');
     assert.equal(err.actual, 'Number');
     assert.equal(err.line, 3);
+
+    var err2 = meta.errors[1];
+    assert.equal(err2.type, 'jsig.sub-type.type-class-mismatch');
+    assert.equal(err2.expected, 'String');
+    assert.equal(err2.actual, 'Number');
+    assert.equal(err2.line, 3);
 
     assert.end();
 });
