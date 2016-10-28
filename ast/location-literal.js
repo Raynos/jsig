607bae86730776532680928a2ff62774672fea00
raynos <raynos2@gmail.com>
2016-10-28 22:35:22 +0100

    ast/location-literal: double check that location is not empty

--- ast/location-literal.js
+++ ast/location-literal.js
@@ -2,9 +2,13 @@
 
 /* @jsig */
 
+var assert = require('assert');
+
 module.exports = LocationLiteralNode;
 
 function LocationLiteralNode(name, location) {
+    assert(location.length > 0, 'location must contain values');
+
     this.type = 'locationLiteral';
     this.name = name;
     this.location = location;
