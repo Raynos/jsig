501c1b53dbf5cd43605996b1dab91cfcd25b2ebe
raynos <raynos2@gmail.com>
2016-11-01 15:02:01 +0000

    tests: add a function overload test

--- test/unit/function-overloading.js
+++ test/unit/function-overloading.js
@@ -217,3 +217,28 @@ JSIGSnippet.test('overloading an event emitter object (sugar)', {
     snippet.compileAndCheck(assert);
     assert.end();
 });
+
+JSIGSnippet.test('overloaded callbacks as func decl', {
+    snippet: function m() {/*
+        db.load('key', onObj);
+
+        function onObj(err, obj) {
+        }
+    */},
+    header: function h() {/*
+        interface Obj {
+            key: String,
+            value: String
+        }
+
+        db : {
+            load: (key: String, cb : (
+                ((err: Error, obj?: Obj) => void) &
+                ((err: null, obj: Obj) => void)
+            )) => void
+        }
+    */}
+}, function t(snippet, assert) {
+    snippet.compileAndCheck(assert);
+    assert.end();
+});
