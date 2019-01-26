2547726aec50a7abfa1cc4e39f7d9097c0ed1cca
Jake Verbaten <raynos2@gmail.com>
2019-01-26 23:18:02 +0100

    second test case for inference

--- test/unit/scenario.batch-client.js
+++ test/unit/scenario.batch-client.js
@@ -26,6 +26,7 @@ JSIGSnippet.test('good working simple constructor', {
 });
 
 JSIGSnippet.test('good calling string x in constructor', {
+    fullInference: true,
     snippet: function m() {/*
         module.exports = BatchClient;
 
@@ -35,23 +36,17 @@ JSIGSnippet.test('good calling string x in constructor', {
 
             this.value = String(10);
         }
-    */},
-    header: function h() {/*
-        type Channel : Object
-        type IBatchClient : {
-            channel: Channel,
-            hosts: Array<String>,
-            value: String
-        }
-
-        BatchClient : (
-            this: IBatchClient,
-            channel: Channel,
-            hosts: Array<String>
-        ) => void
     */}
 }, function t(snippet, assert) {
-    snippet.compileAndCheck(assert);
+    var meta = snippet.compileAndCheck(assert);
+    var exported = meta.serializeType(meta.moduleExportsType);
+
+    assert.equal(exported, '<T0, T1>(this: {\n' +
+        '    channel: T0,\n' +
+        '    hosts: T1,\n' +
+        '    value: String\n' +
+        '}, channel: T0, hosts: T1) => void'
+    );
 
     assert.end();
 });
