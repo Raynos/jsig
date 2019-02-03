1f19f6409b0bafd555840e8d75c37eae99772552
Jake Verbaten <raynos2@gmail.com>
2019-02-03 04:14:30 +0100

    ast/union: disallow empty unions

--- ast/union.js
+++ ast/union.js
@@ -10,6 +10,8 @@ function UnionTypeNode(unions, label, opts) {
     assert(!label, 'cannot have label on union');
     assert(!(opts && opts.optional), 'cannot have optional on union');
 
+    assert(unions.length > 0, 'cannot allocate empty union');
+
     this.type = 'unionType';
     this.unions = unions;
     this._raw = null;
